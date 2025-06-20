3abdf2dec312cf520ca82b04d34a6f0ad8cb3df8
Sherezade Chinarro <serecl1993@hotmail.com>
2025-06-20 11:08:00 +0200

    Agregado formulario inscripcion App

---
+++ formulario-cliente/formulario-cliente.module.ts
@@ -0,0 +1,22 @@
+/*
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { ClientFormComponent } from './client-form.component';
+
+
+@NgModule({
+  declarations: [ClientFormComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+  exports: [ClientFormComponent]
+})
+export class ClientFormModule {} */
