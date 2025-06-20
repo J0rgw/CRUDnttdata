e9c3463a23556dc6ee671b175b5dff7b251fce9f
Joel Vives <joelvivesvidal@gmail.com>
2025-06-20 10:54:58 +0200

    Servicio corregido

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Cliente } from '../models/cliente.model';
 import { Observable } from 'rxjs';
 
@@ -8,26 +8,34 @@ import { Observable } from 'rxjs';
 })
 export class ClienteService {
   private apiUrl = "https://clients-example-api.fly.dev/api/clients";
+  private groupId = "2";
 
   constructor(private http: HttpClient) { }
 
+  private getHeaders(): HttpHeaders {
+  return new HttpHeaders({
+    'X-Group-Id': this.groupId.toString(),
+    'Content-Type': 'application/json'
+  });
+}
+
   getClientes(): Observable<Cliente[]> {
-      return this.http.get<Cliente[]>(this.apiUrl);
+      return this.http.get<Cliente[]>(this.apiUrl, {headers: this.getHeaders()});
   }
 
   createCliente(cliente: Cliente): Observable<Cliente> {
-    return this.http.post<Cliente>(this.apiUrl, cliente);
+    return this.http.post<Cliente>(this.apiUrl, cliente, {headers: this.getHeaders()});
   }
 
   getClienteById(id: number): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.apiUrl}/${id}`);
+    return this.http.get<Cliente>(`${this.apiUrl}/${id}`, {headers: this.getHeaders()});
   }
 
   updateCliente(id: number, cliente: Cliente): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, cliente);
+    return this.http.put(`${this.apiUrl}/${id}`, cliente, {headers: this.getHeaders()});
   }
 
   deleteCliente(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`, {headers: this.getHeaders()});
   }
 }
